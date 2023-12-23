c31364eb7339297372f651e35c448148f049e2cf
Davi <Davi@DESKTOP-F5A76OK>
2023-12-23 11:07:04 -0300

    Adicionar arquivos de projeto.

---
+++ BankTest/ContaCorrente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankTest
+{
+    public class ContaCorrente
+    {
+        public string DadosCompletoDaContaCorrente => $"Titular: {Titular}, Agência: {Agencia}, Numero: {Numero}, Saldo: {Saldo}";
+        public string Titular { get; set; }
+        public int Agencia { get; set; }
+        public int Numero { get; set; }
+        public double Saldo { get; set; }
+
+
+    }
+}
---
+++ BankTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BankTest;
+class Program
+{
+    static void Main(string[] args)
+    {
+        ContaCorrente conta = new();
+        conta.Titular = "davi";
+        conta.Agencia = 863;
+        conta.Numero = 12345676;
+        conta.Saldo = 5000.00;
+
+        Console.WriteLine(conta.DadosCompletoDaContaCorrente);
+    }
+}
